801defaac38bd6bdcae84d21a3df1c79d78c267e
JMJ <yang.jmj@gmail.com>
2021-12-15 13:46:19 +0800

    update

---
+++ bin/learn_dart_with_cli.dart
@@ -0,0 +1,5 @@
+import 'package:learn_dart_with_cli/learn_dart_with_cli.dart' as learn_dart_with_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${learn_dart_with_cli.calculate()}!');
+}
---
+++ test/learn_dart_with_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:learn_dart_with_cli/learn_dart_with_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
